10e95b48d7436ea55a079f0a22b5049b9bd4d463
emiel <emiel@0cc3fc13-06fc-4b91-9775-6014b1a2715b>
2014-10-10 09:05:08 +0000

    {00:10} [#9178] implemented pre-freeze.php to update the version number in the XML file

--- pre-freeze.php
+++ pre-freeze.php
@@ -10,7 +10,37 @@
  *  @copyright 2014 Copernica BV
  *  @documentation private
  */
+require_once('PxFramework/autoload.php');
 
+/**
+ *  Check usage
+ */
+if ($argc < 2) die("usage: pre-freeze.php <version>\n");
 
-echo("pre-freeze.php");
+/**
+ *  The new version number
+ *  @var string
+ */
+$version = $argv[1];
 
+/**
+ *  The XML file that we need to store the version number in
+ *  @var string
+ */
+$xmlfile = __DIR__.'/magento/app/code/community/Copernica/Integration/etc/config.xml';
+
+/**
+ *  Turn the xml file in a SimpleXML object structure
+ *  @var PxSimpleXmlElement
+ */
+$xml = new PxSimpleXmlElement(file_get_contents($xmlfile));
+
+/**
+ *  Update version number
+ */
+$xml->modules->Copernica_Integration->version = $version;
+
+/**
+ *  Store file contents with updated version number
+ */
+file_put_contents($xmlfile, $xml->asXML());
