39d96966a3a46f5843617be6fb9ed38cbfdeb3e5
emiel <emiel@0cc3fc13-06fc-4b91-9775-6014b1a2715b>
2014-10-10 08:54:02 +0000

    {00:10} [#9178] initial attempt to add pre-freeze.php script

---
+++ pre-freeze.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *  pre-freeze.php
+ *
+ *  Script that is automatically started by the codeFreeze command
+ *  right before the code is frozen. This script updates the version
+ *  number in the XML files
+ * 
+ *  @author Emiel Bruijntjes <emiel.bruijntjes@copernica.com>
+ *  @copyright 2014 Copernica BV
+ *  @documentation private
+ */
+
+
+echo("pre-freeze.php");
+
