57e4e859e895c795a6eb43cf3fafb453fa78ed9f
pawel <pawel@pawel-Latitude-E6530.(none)>
2015-01-20 13:35:15 +0100

    Fixed build script. Build script will ignore .git directories. Build script will output less informations. Build script can be executed from any directory. Still, zip archive will be generated in side repository root directory.

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-# remove old magento extension
-rm magento_ext.zip
+# get proper script path
+SCRIPT=$(readlink -f "$0")
 
-# we want to zip whole dir but exclude .svn directories
-zip -r magento_ext.zip magento --exclude=*.svn* 
+# find out repository base dir
+BASEDIR=$(dirname "$SCRIPT")
+
+# remove old extension package (if one exists)
+if [ -f $BASEDIR/copernica_magento_extension.zip ] 
+then
+    rm $BASEDIR/copernica_magento_extension.zip
+fi
+
+# we want to zip whole dir but exclude .git directories directories
+zip -r $BASEDIR/copernica_magento_extension.zip $BASEDIR/magento --exclude=*.git* > /dev/null
